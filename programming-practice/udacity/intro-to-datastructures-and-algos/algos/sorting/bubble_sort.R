6eb31a9220014f41edfe8cf545b69e9e547463e6
v-adhithyan <aavispeaks@gmail.com>
2018-09-04 11:08:49 +0530

    add bubble sort visualization

--- programming-practice/udacity/intro-to-datastructures-and-algos/algos/sorting/bubble_sort.R
+++ programming-practice/udacity/intro-to-datastructures-and-algos/algos/sorting/bubble_sort.R
@@ -1,26 +1,30 @@
-swap <- function(i, j, data) {
-  temp <- data[i]
-  data[i] <- data[j]
-  data[j] <- temp
-  return data
-}
+
 
 sleep <- function() {
   current_time <- Sys.time()
   while(as.numeric(Sys.time()) - as.numeric(current_time) < 0.5) {}
 }
 
-input <- c(10, 4, 3, 5, 1, 7, 6, 2, 8, 9)
-barplot(input)
-
-for(i in 1:length(input)) {
-  j = i + 1
-  while(j <= length(input)) {
-    if(input[i] > input[j]) {
-      input <- swap(i, j, input)
-      barplot(input)
+bubble_sort <- function(input) {
+  for(i in 1:length(input)) {
+    j = i + 1
+    while(j <= length(input)) {
+      if(input[i] > input[j]) {
+        temp <- input[i]
+        input[i] <- input[j]
+        input[j] <- temp
+        barplot(input)
+      }
+      j = j + 1
+      sleep()
     }
-    j = j + 1
-    sleep()
   }
 }
+
+main <- function() {
+  input <- c(10, 4, 3, 5, 1, 7, 6, 2, 8, 9)
+  barplot(input)
+  bubble_sort(input)
+}
+
+main()
